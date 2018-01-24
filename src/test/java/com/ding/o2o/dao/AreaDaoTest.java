f24c3e2bafa7be763e926165afc2c9bef947e55e
dj940212 <2902273280@qq.com>
2018-01-24 15:37:33 +0800

    dao验证成功

--- src/test/java/com/ding/o2o/dao/AreaDaoTest.java
+++ src/test/java/com/ding/o2o/dao/AreaDaoTest.java
@@ -1,5 +1,21 @@
 package com.ding.o2o.dao;
 
-public class AreaDaoTest {
-    
+import com.ding.o2o.BaseTest;
+import com.ding.o2o.entity.Area;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class AreaDaoTest extends BaseTest{
+    @Autowired
+    private AreaDao areaDao;
+
+    @Test
+    public void testQueryArea() {
+        List<Area> areaList = areaDao.queryArea();
+        assertEquals(4, areaList.size());
+    }
 }
